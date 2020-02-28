294da1810ec96b3653575531b7b4af91821881f8
albertw4 <liangalbert0303@gmail.com>
2020-02-28 00:25:32 -0800

    Basic server and client is working, client can sent message to server, and server can see message

--- client/client.c
+++ client/client.c
@@ -1,8 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+typedef struct sockaddr SA;
 
-int main()
+// Opens a socket
+int open_clientfd(char* hostname, char* portNum){
+    int clientSocket;
+    struct addrinfo hints, *results, *p;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags |= AI_ADDRCONFIG;
+
+    if((getaddrinfo(hostname, portNum, &hints, &results)) != 0){
+        printf("Error Translating Socket\n");
+        return -1;
+    }
+    printf("Socket Translated\n");
+
+    for(p = results; p!= NULL; p = p->ai_next){
+        clientSocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if(clientSocket < 0) continue;
+        if(connect(clientSocket, p->ai_addr, p->ai_addrlen) != -1) break;
+        close(clientSocket);
+    }
+
+    freeaddrinfo(results);
+
+    if(p == NULL) return -1;
+    else{
+        printf("Socket Created Successful\n");
+        return clientSocket;
+    }
+
+}
+
+int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    struct sockaddr_in server_address;
+    int clientfd;
+    int MAXLINE = 256;
+    char* command;
+    char* hostName = "localhost"; //change to argv[1] later
+    char* portNum = "30000";    //change to argv[2] later
+    char input[MAXLINE];
+
+    clientfd = open_clientfd(hostName,portNum);
+
+    while(1){
+        printf("\n> ");
+        fgets(input,MAXLINE,stdin);
+        char *newline = strchr(input, '\n');
+        if(newline){*newline = 0;}
+        command = strtok(input, " ");
+
+        printf("Command is |%s|, length is %d\n", command, strlen(command));
+
+        if(strcmp(command, "quit") == 0){
+            break;
+        }
+
+        write(clientfd, command, strlen(command));
+        read(clientfd, command, MAXLINE);
+        fputs(command,stdout);
+
+    }
+
+    close(clientfd);
+
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -2,34 +2,81 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
 typedef struct sockaddr SA;
+int MAXLINE = 256;
+
+
+int open_listenfd(char* portNum){
+    int serverSocket;
+    int optval = 1;
+    struct addrinfo hints, *results, *p;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags |= AI_ADDRCONFIG;
+
+    if((getaddrinfo(NULL, portNum, &hints, &results)) != 0){
+        printf("Error Translating Socket\n");
+        return -1;
+    }
+    printf("Socket Translated\n");
+
+    for(p = results; p!= NULL; p = p->ai_next){
+        serverSocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if(serverSocket < 0) continue;
+        setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR,(const void *)&optval , sizeof(int));
+        if (bind(serverSocket, p->ai_addr, p->ai_addrlen) == 0) break; /* Success */
+        close(serverSocket);
+    }
+
+    freeaddrinfo(results);
+
+    if(p == NULL) return -1;
+    if(listen(serverSocket, optval) < 0){
+        close(serverSocket);
+        return -1;
+    }
+
+    printf("Socket Created and Bounded Successful\n");
+
+    return serverSocket;
+}
+
+void echo(int connfd){
+    size_t n;
+    char buf[MAXLINE];
+    while((n = read(connfd, buf, MAXLINE)) != 0) {
+        printf("server received %d bytes\n", (int)n);
+        write(connfd, buf, n);
+    }
+}
 
 int main()
 {
-    char server_message[250] = "Connected to Server BOI!";
+    int listenfd, clientSocket;
+    socklen_t clientLen;
+    struct sockaddr_storage clientAddr;
+    char* portNum = "30000";
+    char* host = "localhost";
+    char client_hostname[MAXLINE], client_port[MAXLINE];
 
-    int server_socket;
-
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    //get server address
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(30000);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    //bind socket to our ip and port
-    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-    listen(server_socket, 3);
-
-    int client_socket;
-
-    accept()
-
+    while(1){
+        listenfd = open_listenfd(portNum); //change to argv[1] later
+        clientLen = sizeof(struct sockaddr_storage);
+        clientSocket = accept(listenfd, (SA *)&clientAddr, &clientLen);
+        getnameinfo((SA *) &clientAddr, clientLen,client_hostname, MAXLINE, client_port, MAXLINE, 0);
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        echo(clientSocket);
+        close(clientSocket);
+    }
 
     return 0;
 }
